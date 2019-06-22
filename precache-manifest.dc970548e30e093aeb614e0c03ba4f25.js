c0762fa2cc95580c6e9557dc8d1e22f0d90be292
Cory Dissinger <cory.dissinger@gmail.com>
2019-06-22 16:41:38 -0400

    Updates

---
+++ precache-manifest.dc970548e30e093aeb614e0c03ba4f25.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0f0064c980471ea0dfbd4cf19ac183a0",
+    "url": "/cra-go-demo-board/index.html"
+  },
+  {
+    "revision": "ab72376017262d7b8d1d",
+    "url": "/cra-go-demo-board/static/css/main.7debb425.chunk.css"
+  },
+  {
+    "revision": "415a4618890862b89c3b",
+    "url": "/cra-go-demo-board/static/js/2.b69743d4.chunk.js"
+  },
+  {
+    "revision": "ab72376017262d7b8d1d",
+    "url": "/cra-go-demo-board/static/js/main.df7c394c.chunk.js"
+  },
+  {
+    "revision": "92a34c2812313507bc70",
+    "url": "/cra-go-demo-board/static/js/runtime~main.67231506.js"
+  }
+]);
